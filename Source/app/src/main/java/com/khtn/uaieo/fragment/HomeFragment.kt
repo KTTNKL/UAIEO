1e5c8e5ea0ae86e328a074edb75475da1cb38a1f
luong07734 <le.ky.luong@gmail.com>
2022-03-26 11:33:10 +0700

    [19127208] Code Quiz feature, Dictionary UI, Home View Images

--- Source/app/src/main/java/com/khtn/uaieo/fragment/HomeFragment.kt
+++ Source/app/src/main/java/com/khtn/uaieo/fragment/HomeFragment.kt
@@ -37,10 +37,12 @@ class HomeFragment : Fragment() {
         }
         var itemList:ArrayList<itemMenu>
         itemList= ArrayList()
-        itemList.add(itemMenu(R.drawable.bg_quiz,"Quiz","Practice Vocab"))
-        itemList.add(itemMenu(R.drawable.bg_quiz,"Quiz","Practice Vocab"))
-        itemList.add(itemMenu(R.drawable.bg_quiz,"Quiz","Practice Vocab"))
-        itemList.add(itemMenu(R.drawable.bg_quiz,"Quiz","Practice Vocab"))
+        itemList.add(itemMenu(R.drawable.bg_news,"Đọc báo","Luyện tập kỹ năng đọc"))
+        itemList.add(itemMenu(R.drawable.bg_sub,"Vietsub","Dịch các bài đọc mẫu"))
+        itemList.add(itemMenu(R.drawable.bg_schedule,"Lập lịch","Lâp lịch thi TOEIC"))
+        itemList.add(itemMenu(R.drawable.bg_tip,"Mẹo","Mẹo TOEIC hữu ích"))
+        itemList.add(itemMenu(R.drawable.bg_quiz,"Quiz","Trò chơi luyện từ vựng"))
+        itemList.add(itemMenu(R.drawable.bg_dic,"Từ điển","Tra cứu từ khó"))
 
         adapter= MenuAdapter(itemList)
         if (recyclerView != null) {
