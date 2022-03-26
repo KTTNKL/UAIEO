2eeebfbf65ce06eef0126bddbfef0ae871e63c75
TrungNam <huynhtrungnam2001@gmail.com>
2022-03-26 16:15:02 +0700

    [19127478] fixButtonVietsub + TipActivity

---
+++ Source/app/src/main/java/com/khtn/uaieo/activity/TipDetailActivity.kt
@@ -0,0 +1,65 @@
+package com.khtn.uaieo.activity
+
+import android.app.ProgressDialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
+import android.util.Log
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.khtn.uaieo.R
+import com.khtn.uaieo.adapter.TipAdapter
+import com.khtn.uaieo.model.Tip
+import kotlinx.android.synthetic.main.activity_tip_detail.*
+import kotlinx.android.synthetic.main.activity_vietsub.*
+
+class TipDetailActivity : AppCompatActivity() {
+    lateinit var readingArrayList: ArrayList<Tip>
+    lateinit var dialog: ProgressDialog
+    var message: String = ""
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tip_detail)
+
+        val intent = intent
+        message = intent.getStringExtra("TipIndex").toString()
+
+        readingArrayList = ArrayList<Tip>()
+
+        tipTitleDetailTV.setMovementMethod(ScrollingMovementMethod())
+        tipContentTV.setMovementMethod(ScrollingMovementMethod())
+        dialog = ProgressDialog.show(
+            this, "",
+            "Loading Reading. Please wait...", true
+        )
+        LoadData()
+    }
+
+    fun LoadData(){
+        val ref= FirebaseDatabase.getInstance().getReference("tips")
+        ref.addListenerForSingleValueEvent(object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                //Xoa list trc khi them vao moi lan vao app
+                readingArrayList.clear()
+                for (vietsub in snapshot.children){
+                    val modelTip = vietsub.getValue(Tip::class.java)
+                    if (modelTip != null) {
+                        modelTip.createSubtitle()
+                    }
+                    readingArrayList.add(modelTip!!)
+                }
+                dialog.dismiss()
+
+                tipTitleDetailTV.text = readingArrayList[message.toInt()].title
+                tipContentTV.text = readingArrayList[message.toInt()].content
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
+    }
+}
--- Source/app/src/main/java/com/khtn/uaieo/activity/VietsubActivity.kt
+++ Source/app/src/main/java/com/khtn/uaieo/activity/VietsubActivity.kt
@@ -3,6 +3,7 @@ import android.app.ProgressDialog
 import android.os.Bundle
 import android.text.method.ScrollingMovementMethod
 import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
@@ -29,11 +30,18 @@ class VietsubActivity : AppCompatActivity() {
             this, "",
             "Loading Reading. Please wait...", true
         )
-        LoadData()
 
+        backBtn.visibility= View.INVISIBLE
+
+
+        LoadData()
         backBtn.setOnClickListener{
             if(currentIndex > 0){
                 currentIndex--;
+
+                nextBtn.visibility= View.VISIBLE
+                if(currentIndex == 0)   backBtn.visibility= View.INVISIBLE
+
                 titleReadingTV.text = readingArrayList.get(currentIndex).title
                 EnReadingTV.text = readingArrayList.get(currentIndex).enReading
                 ViReadingTV.text = readingArrayList.get(currentIndex).viReading
@@ -43,6 +51,10 @@ class VietsubActivity : AppCompatActivity() {
         nextBtn.setOnClickListener{
             if(currentIndex < (totalReading-1)){
                 currentIndex++;
+
+                backBtn.visibility= View.VISIBLE
+                if(currentIndex == (totalReading-1))   nextBtn.visibility= View.INVISIBLE
+
                 titleReadingTV.text = readingArrayList.get(currentIndex).title
                 EnReadingTV.text = readingArrayList.get(currentIndex).enReading
                 ViReadingTV.text = readingArrayList.get(currentIndex).viReading
