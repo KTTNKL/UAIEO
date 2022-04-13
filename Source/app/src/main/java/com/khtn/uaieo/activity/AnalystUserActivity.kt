a7e95c176c1786b3263ee935050224adb7337949
19127312 <19127312@student.hcmus.edu.vn>
2022-04-13 16:24:08 +0700

    [19127312] Analyst

--- Source/app/src/main/java/com/khtn/uaieo/activity/AnalystUserActivity.kt
+++ Source/app/src/main/java/com/khtn/uaieo/activity/AnalystUserActivity.kt
@@ -3,17 +3,24 @@ package com.khtn.uaieo.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import com.khtn.uaieo.R
+import com.khtn.uaieo.adapter.AnalystUserAdapter
+import com.khtn.uaieo.adapter.TipAdapter
 import com.khtn.uaieo.model.itemAnalystUser
 import com.khtn.uaieo.model.itemExamRL
 
 class AnalystUserActivity : AppCompatActivity() {
     var id=""
     var arrUser= ArrayList<itemAnalystUser>()
+    lateinit var RV: RecyclerView
+    lateinit var adapter:AnalystUserAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_analyst_user)
@@ -23,7 +30,10 @@ class AnalystUserActivity : AppCompatActivity() {
     }
 
     private fun setupLayout() {
-
+        adapter=AnalystUserAdapter(arrUser)
+        RV = findViewById(R.id.anlystUserRV)
+        RV.layoutManager = LinearLayoutManager(this)
+        RV.adapter = adapter
     }
 
     private fun loadData() {
@@ -36,7 +46,7 @@ class AnalystUserActivity : AppCompatActivity() {
                     val modelUser = item.getValue(itemAnalystUser::class.java)
                     arrUser.add(modelUser!!)
                 }
-
+                adapter.notifyDataSetChanged()
 
             }
 
--- Source/app/src/main/java/com/khtn/uaieo/adapter/AnalystUserAdapter.kt
+++ Source/app/src/main/java/com/khtn/uaieo/adapter/AnalystUserAdapter.kt
@@ -7,30 +7,45 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.khtn.uaieo.R
 import com.khtn.uaieo.model.Tip
+import com.khtn.uaieo.model.itemAnalystUser
 
-class AnalystUserAdapter(private val tipList: ArrayList<Tip>):
+class AnalystUserAdapter(private val userList: ArrayList<itemAnalystUser>):
     RecyclerView.Adapter<AnalystUserAdapter.MyViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.tip_list_items, parent, false);
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_analyst_user, parent, false);
         return MyViewHolder(itemView)
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        var currentItem = tipList[position]
-        holder.title.text = currentItem.title
-        holder.subtitle.text = currentItem.subtitle
+        var currentItem = userList[position]
+
+        holder.email.text=currentItem.email
+        holder.part1.text=currentItem.part1.toString()
+        holder.part2.text=currentItem.part2.toString()
+        holder.part3.text=currentItem.part3.toString()
+        holder.part4.text=currentItem.part4.toString()
+        holder.part5.text=currentItem.part5.toString()
+        holder.part6.text=currentItem.part6.toString()
+        holder.part7.text=currentItem.part7.toString()
 
     }
 
     override fun getItemCount(): Int {
-        return tipList.size;
+        return userList.size;
     }
 
     class MyViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
 
-        val title: TextView = itemView.findViewById(R.id.tipTitleTV)
-        val subtitle: TextView = itemView.findViewById(R.id.subtitleTV)
+        val email: TextView = itemView.findViewById(R.id.emailUserTV)
+        val overall: TextView = itemView.findViewById(R.id.overallTV)
+        val part1: TextView = itemView.findViewById(R.id.p1TV)
+        val part2: TextView = itemView.findViewById(R.id.p2TV)
+        val part3: TextView = itemView.findViewById(R.id.p3TV)
+        val part4: TextView = itemView.findViewById(R.id.p4TV)
+        val part5: TextView = itemView.findViewById(R.id.p5TV)
+        val part6: TextView = itemView.findViewById(R.id.p6TV)
+        val part7: TextView = itemView.findViewById(R.id.p7TV)
 
     }
 
