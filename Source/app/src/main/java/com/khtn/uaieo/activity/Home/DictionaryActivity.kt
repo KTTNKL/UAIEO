5f6e4157b110c04c07eaded5c3772a9f9f97050b
luong07734 <le.ky.luong@gmail.com>
2022-04-26 16:09:30 +0700

    [19127208] Fix UI

--- Source/app/src/main/java/com/khtn/uaieo/activity/Home/DictionaryActivity.kt
+++ Source/app/src/main/java/com/khtn/uaieo/activity/Home/DictionaryActivity.kt
@@ -1,6 +1,8 @@
 package com.khtn.uaieo.activity.Home
 
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 import com.khtn.uaieo.DictionaryAPI
 import com.khtn.uaieo.R
@@ -12,22 +14,23 @@ class DictionaryActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dictionary)
 
-//        webView.settings.setJavaScriptEnabled(true)
-//
-//        webView.webViewClient = object : WebViewClient() {
-//            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
-//                url?.let { view?.loadUrl(it) }
-//                return true
-//            }
-//        }
-//        webView.loadUrl("https://dictionary.cambridge.org/vi/dictionary/english/dictionary")
-//        searchButton.setOnClickListener {
-//            webView.loadUrl("https://dictionary.cambridge.org/vi/dictionary/english/"+SearchBar.text)
-//        }
+        webView.settings.setJavaScriptEnabled(true)
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                url?.let { view?.loadUrl(it) }
+                return true
+            }
+        }
+
+
         searchButton.setOnClickListener {
+            symTV.text = "TỪ ĐIỂN TỪ ĐỒNG NGHĨA"
             var query= SearchBar.text.toString()
             var myDic= DictionaryAPI(this,query,translateTV)
             myDic.execute()
+            webView.loadUrl("https://www.thesaurus.com/")
+            webView.loadUrl("https://www.thesaurus.com/browse/"+SearchBar.text)
         }
     }
 }
